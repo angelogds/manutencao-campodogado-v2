be887643800e0e4a721f7972e4ccb20bdfb3dcad
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-05 20:11:48 -0300

    Update usuarios.routes.js

--- modules/usuarios/usuarios.routes.js
+++ modules/usuarios/usuarios.routes.js
@@ -4,16 +4,6 @@ const router = express.Router();
 const { requireLogin, requireRole } = require("../auth/auth.middleware");
 const controller = require("./usuarios.controller");
 
-// tudo em /admin/users (ADMIN)
-router.use("/admin/users", requireLogin, requireRole(["ADMIN"]));
-
-router.get("/admin/users", controller.list);
-router.get("/admin/users/new", controller.newForm);
-router.post("/admin/users", controller.create);
-
-router.get("/admin/users/:id/edit", controller.editForm);
-router.post("/admin/users/:id", controller.update);
-
-router.post("/admin/users/:id/reset-password", controller.resetPassword);
+router.get("/admin/users", requireLogin, requireRole(["ADMIN"]), controller.list);
 
 module.exports = router;
